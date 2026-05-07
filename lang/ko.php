6c5bcfc077583f54ab02c2f4397dc3150359bf97
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for sugar-glow.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'render.unknown_theme' => '알 수 없는 테마: {name}',
+];
