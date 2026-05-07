9fc48660c924bff64c591dd7b51def05581f65a6
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Japanese translations for sugar-glow.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'render.unknown_theme' => '不明なテーマ：{name}',
+];
